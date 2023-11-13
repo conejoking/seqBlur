544678a69fffe24fab1c2edc90e073f3fb88a398
naavioce <carie.navio@gmail.com>
2023-11-12 23:25:16 -0700

    adding CUDA version in main.cu that segfaults on line 151

---
+++ .vscode/main.cu
@@ -0,0 +1,200 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <vector>
+#include <chrono>
+#include <fstream>
+#include <inttypes.h>
+
+// 240p : 426 width 240 height
+// 360p : 640 width 360 height
+// 480p : 854 width 480 height
+// 720p : 1280 width 720 height
+// 1080p : 1920 width 1080 height
+
+//1024 threads per block
+// 2^32 - 1 x 65,535 x 65,535 in (x,y,z)
+
+#pragma pack(push, 1) // Ensure structure alignment
+struct BMPHeader {
+    uint16_t fileType;
+    uint32_t fileSize;
+    uint16_t reserved1;
+    uint16_t reserved2;
+    uint32_t dataOffset;
+    uint32_t headerSize;
+    int32_t width;
+    int32_t height;
+    uint16_t planes;
+    uint16_t bitsPerPixel;
+    uint32_t compression;
+    uint32_t imageSize;
+    int32_t xPixelsPerMeter;
+    int32_t yPixelsPerMeter;
+    uint32_t totalColors;
+    uint32_t importantColors;
+};
+#pragma pack(pop)
+
+typedef struct Pixel { // BMP stores pixel data in BGR order
+    unsigned char blue;
+    unsigned char green;
+    unsigned char red; 
+    unsigned char reserved;
+}Pixel;
+
+__host__ __device__ void setPixel(unsigned char * image, int width, int height, Pixel* pixel)
+{
+    image[0] = pixel->blue;
+    image[1] = pixel->green;
+    image[2] = pixel->red;
+}
+
+__host__ __device__ void getPixel(unsigned char * image, int width, int height, Pixel* pixel)
+{
+    pixel->blue = image[0];
+    pixel->green = image[1];
+    pixel->red = image[2];
+}
+
+__global__ void averageBlur(unsigned char* image_in, unsigned char* image_out, int width, int height, int radius)
+{
+    int x = threadIdx.x + blockDim.x * blockIdx.x;
+    int y = threadIdx.y + blockDim.y * blockIdx.y;
+
+    int x_dir = (x / width);
+    int y_dir = (y / height);
+
+    int totalPixels = 0;
+    int totalRed = 0, totalGreen = 0, totalBlue = 0;
+    Pixel inputPixel, outputPixel;
+    for (int i = -radius; i < radius; i++)
+    {
+        for (int j = -radius; j < radius; j++)
+        {
+            int newWidth = x_dir + i;
+            int newHeight = y_dir + j;
+            if (newWidth >= 0 && newWidth < width && newHeight >= 0 && newHeight < height)
+            {
+                getPixel(image_in, newWidth, newHeight, &inputPixel);
+                totalRed += inputPixel.red;
+                totalGreen += inputPixel.green;
+                totalBlue += inputPixel.blue;
+                ++totalPixels;
+            }
+        }
+    }
+    outputPixel.red = static_cast<unsigned char>(totalRed / totalPixels);
+    outputPixel.green = static_cast<unsigned char>(totalGreen / totalPixels);
+    outputPixel.blue = static_cast<unsigned char>(totalBlue / totalPixels);
+
+    setPixel(image_out, x_dir, y_dir, &outputPixel);
+}
+
+void save_image(unsigned char * imagePixel, int width, int height, const char * filename, BMPHeader headerInfo)
+{
+    // Write the blurred image to a new file
+    std::ofstream outputFile(filename, std::ios::binary);
+
+    if (!outputFile) {
+        throw std::invalid_argument("Error: Couldn't create the output BMP image file.");
+    }
+
+    // Write BMP header to the output file
+    outputFile.write(reinterpret_cast<char*>(&headerInfo), sizeof(BMPHeader));
+
+    std::vector<unsigned char> outputPixels;
+
+    for (int j = 0; j < height; j++)
+    {
+        for (int i = 0; i < width; i++)
+        {
+            outputPixels.push_back(imagePixel[(j * width) + i]);
+        }
+
+        // Add any needed padding to align rows to multiples of 4 bytes
+        for (int p = 0; p < (4 - (width * 3) % 4) % 4; ++p) {
+            outputPixels.push_back(0);
+        }
+    }
+
+    outputFile.write((const char*)& outputPixels[0], outputPixels.size());
+    // Close the output file
+    outputFile.close();
+}
+
+const BMPHeader create_image(std::vector<unsigned char>& image_copy, int width, int height, const char* inputFilePath)
+{
+    std::ifstream inputFile(inputFilePath, std::ios::binary);
+
+    if (!inputFile) 
+    {
+        throw std::invalid_argument("Error: Couldn't open the input BMP image file.");
+    }
+
+    // Read BMP header
+    BMPHeader headerInfo;
+    inputFile.read(reinterpret_cast<char*>(&headerInfo), sizeof(BMPHeader));
+
+    // Check if the file is a valid BMP file
+    if (headerInfo.fileType != 0x4D42 || headerInfo.bitsPerPixel != 24)
+    {
+        inputFile.close();
+        throw std::invalid_argument("Error: Not a valid BMP file or unsupported color depth.");
+    }
+
+    // Move to the beginning of the pixel data
+    inputFile.seekg(headerInfo.dataOffset, std::ios::beg);
+    int length = inputFile.tellg();
+    printf("Image creation complete at offset: %d\n", length);
+    
+    // TODO: THIS IS CAUSING A SEGFAULT
+    inputFile.read((char*)(&image_copy[0]), length);
+
+    inputFile.close();
+    return headerInfo;
+}
+
+int main(int argc, char * argv[]) 
+{   
+    if (argc < 4)
+    {
+        printf("not enough inputs. See ReadMe");
+    }
+    const char* filename = argv[1];
+    int width = atoi(argv[2]);
+    int height = atoi(argv[3]);
+    int radius = atoi(argv[4]);
+
+    std::vector<unsigned char> input_image;
+
+    BMPHeader headerInfo = create_image(input_image, width, height, filename);
+    unsigned char* pic_in;
+    unsigned char* pic_out; 
+
+    return 0;
+    int numbytes = sizeof(unsigned char) * width * height * 4;
+
+    cudaMalloc(&pic_in, numbytes);
+    cudaMalloc(&pic_out, numbytes);
+    cudaMemcpy(pic_in, &input_image[0], numbytes, cudaMemcpyHostToDevice);
+
+    dim3 threadsPerBlock(32, 32); // 1024 threads per block
+    dim3 numBlocks(width/threadsPerBlock.x, height/threadsPerBlock.y); 
+
+    double start = (1000 * ((double)clock())) / (double)CLOCKS_PER_SEC;
+    averageBlur << <numBlocks, threadsPerBlock >> > (pic_in, pic_out, width, height, radius);
+    
+    //cudaDeviceSynchronize();
+    unsigned char output_image[numbytes];
+    cudaMemcpy(output_image, pic_out, numbytes, cudaMemcpyHostToDevice);
+
+    save_image(output_image, width, height, "filename.bmp", headerInfo);
+    double end = (1000 * ((double)clock())) / (double)CLOCKS_PER_SEC;
+    double time_taken = end - start;
+
+    printf("Elapsed time for %d x %d resolution image with coeffient of %d: %f \n", width, height, radius, time_taken);
+
+    return 0;
+}
+
+
